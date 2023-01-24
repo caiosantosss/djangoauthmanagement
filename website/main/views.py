a839a17505f433f48bfb45c63c3e2db77f90e239
caiosantosss <caio.santos@msn.com>
2023-01-24 17:47:38 +0900

    add sign up

--- website/main/views.py
+++ website/main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {'form': form})
