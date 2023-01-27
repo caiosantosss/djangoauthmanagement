9f7ff34d79ed2b6cc75c2e6426b9420ba1785537
caiosantosss <caio.santos@msn.com>
2023-01-27 18:03:22 +0900

    add save the user, login and redirect

--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,10 @@ def home(request):
 def sign_up(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
     else:
         form = RegisterForm()
 
