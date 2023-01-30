4277202763cc4266317c9ecee39c3af5e7067eb1
caiosantosss <caio.santos@msn.com>
2023-01-30 15:52:43 +0900

    add create post views.py

--- website/main/views.py
+++ website/main/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
+
 
 # Create your views here.
 
 @login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
+
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/home')
+    else:
+        form = PostForm()
+
+    return render(request, 'main/create_post.html', {'form': form})
 
 def sign_up(request):
     if request.method == 'POST':
